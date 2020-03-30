7d05e94d39cef6027b22bce3f0be1a1430062ea0
JohnEnevoldsen <62882814+JohnEnevoldsen@users.noreply.github.com>
2020-03-30 13:51:08 +0200

    Initial commit

---
+++ Axe.cs
@@ -0,0 +1,29 @@
+﻿namespace WeaponShopV10
+{
+    /// <summary>
+    /// This class represents a Axe. An Axe is 
+    /// considered to be a weapon.
+    /// </summary>
+    public class Axe : Weapon
+    {
+        public const int InitialAxeMinDamage = 20;
+        public const int InitialAxeMaxDamage = 50;
+
+        #region Constructor
+        public Axe(string description) 
+            : base(description, InitialAxeMinDamage, InitialAxeMaxDamage)
+        {
+        } 
+        #endregion
+        public int DamageFromAxe(){
+            int damageToDeal = base.CalculateDamage();
+            base.MinDamage -= 3;
+            base.MaxDamage -= 3;
+            return damageToDeal;
+        }
+        public void Sharpen(){
+            base.MinDamage = InitialAxeMinDamage;
+            base.MaxDamage = InitialAxeMaxDamage;
+        }
+    }
+}
---
+++ Wand.cs
@@ -0,0 +1,32 @@
+﻿namespace WeaponShopV10
+{
+    /// <summary>
+    /// This class represents a Wand. A Wand is 
+    /// considered to be a weapon.
+    /// </summary>
+    public class Wand : Weapon
+    {
+        private bool _isEnchanted;
+        public const int InitialWandMinDamage = 10;
+        public const int InitialWandMaxDamage = 30;
+
+        public bool IsEnchanted{
+            get{return _isEnchanted;}
+            set{_isEnchanted = value;}
+        }
+
+        #region Constructor
+        public Wand(string description) 
+            : base(description, InitialWandMinDamage, InitialWandMaxDamage)
+        {
+            _isEnchanted = false;
+        } 
+        #endregion
+        public int DamageFromWand(){
+            if(_isEnchanted){
+                return base.CalculateDamage() * 2;
+            }
+            return base.CalculateDamage();
+        }
+    }
+}
